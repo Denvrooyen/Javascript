a42e844afcbb4e4039123ae67f4a4660d35b2816
Denvrooyen <denvrooyen@gmail.com>
2018-11-15 21:00:55 +0200

    Added Comments

--- datatypes.js
+++ datatypes.js
@@ -1,15 +1,17 @@
 
-
+// Datatype - Number
 let x = 7;
 console.log(typeof x);
 
+// Datatype - Boolean
 let y = true; 
 console.log(typeof y);
 
+// Datatype - string
 let z = 'hello world'; 
 console.log(typeof z);
 
-
+// Datatype - undefined
 let a;
 console.log(a);
 console.log(typeof a);
