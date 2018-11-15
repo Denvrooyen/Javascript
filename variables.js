5ec7357238337beb304a095d8df26b0c7773de04
Denvrooyen <denvrooyen@gmail.com>
2018-11-15 20:39:44 +0200

    First Commit

---
+++ variables.js
@@ -0,0 +1,11 @@
+//let is an instruction to inform Javascript that we want to take action we want to do something;
+//var - abonden rather use let (const- we do not to intend that it should never change)
+// declare a variable once but set the value as many times as you want with let
+
+// rules for naming identifiers - 1 all variables begin letter dollar sign or number underscore-  can't have any spaces. no keywords. can't use identifier or keyword as a variables
+// case sensitive.
+// codeconvension - need to be description- represent the thing you are storing, camelcasing. Consistant when naming pick one style
+let x = 7;  //creating a variable by putting in the word let and naming it X(creating a space to store thing and we use X to referance to that storge that we created.)Identifier
+let y = 3;
+let z = x + y;
+console.log('Answer:' + z); 
